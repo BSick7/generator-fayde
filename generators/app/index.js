43fc82d4bb5479171355cd763662f9dca567f755
BSick7 <brad.sickles@gmail.com>
2014-07-19 10:41:16 -0400

    Added ability to include Fayde.Controls library.

--- generators/app/index.js
+++ generators/app/index.js
@@ -16,12 +16,42 @@ module.exports = yeoman.generators.Base.extend({
                 this.log(answers.name);
                 done();
             }.bind(this));
+        },
+        promptModules: function () {
+            var done = this.async();
+
+            var prompts = [
+                {
+                    type: 'checkbox',
+                    name: 'modules',
+                    message: 'Which modules would you like to include?',
+                    choices: [
+                        {
+                            value: 'controlsModule',
+                            name: 'Fayde-Controls',
+                            checked: true
+                        }
+                    ]
+                }
+            ];
+
+            this.prompt(prompts, function (props) {
+                var hasMod = function (mod) {
+                    return props.modules.indexOf(mod) !== -1;
+                };
+                this.controlsModule = hasMod('controlsModule');
+                done();
+            }.bind(this));
         }
     },
     configuring: {
         bowerSetup: function () {
             this.copy('_bowerrc', '.bowerrc');
-            this.copy('_bower.json', 'bower.json');
+            if (this.controlsModule) {
+                this.copy('_bower.ex.json', 'bower.json');
+            } else {
+                this.copy('_bower.json', 'bower.json');
+            }
         },
         gruntSetup: function () {
             this.copy('_package.json', 'package.json');
@@ -30,7 +60,11 @@ module.exports = yeoman.generators.Base.extend({
     },
     writing: {
         appFiles: function () {
-            this.copy('app/default.html', 'app/default.html');
+            if (this.controlsModule) {
+                this.copy('app/default.ex.html', 'app/default.html');
+            } else {
+                this.copy('app/default.html', 'app/default.html');
+            }
             this.copy('app/default.fap', 'app/default.fap');
             this.copy('app/ViewModels/MainViewModel.ts', 'app/ViewModels/MainViewModel.ts');
         }
