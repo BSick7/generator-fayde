5e98b1483e4c589bd3fe3a2172a12af805b2b6e3
BSick7 <brad.sickles@gmail.com>
2014-07-26 09:32:30 -0400

    Added `grunt serve` to generated app.

--- generators/app/templates/_Gruntfile.js
+++ generators/app/templates/_Gruntfile.js
@@ -1,7 +1,16 @@
 module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-typescript');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-open');
+
+    var ports = {
+        server: 8000,
+        livereload: 35353
+    };
 
     grunt.initConfig({
+        ports: ports,
         typescript: {
             build: {
                 src: ['app/**/*.ts', '!app/lib/**/*.ts'],
@@ -12,11 +21,33 @@ module.exports = function (grunt) {
                 }
             }
         },
+        connect: {
+            server: {
+                options: {
+                    port: ports.server,
+                    base: './app/'
+                }
+            }
+        },
+        open: {
+            serve: {
+                path: 'http://localhost:<%= ports.server %>/default.html'
+            }
+        },
         watch: {
-            files: 'app/**/*.ts',
-            tasks: ['typescript:build']
+            src: {
+                files: ['app/**/*.ts'],
+                tasks: ['typescript:build']
+            },
+            views: {
+                files: ['app/**/*.fap', 'app/**/*.fayde'],
+                options: {
+                    livereload: ports.livereload
+                }
+            }
         }
     });
 
     grunt.registerTask('default', ['typescript:build']);
+    grunt.registerTask('serve', ['typescript:build', 'connect', 'open', 'watch'])
 };
